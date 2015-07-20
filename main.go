9d4d46de45c6902159b5ab6c7f52345e853e4eda
Kanai Masumi <masumi.net@gmail.com>
2015-07-20 11:20:45 +0900

    renamed:    contorollers/main.go -> controllers/main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/masu-mi/face_detector/contorollers"
+	"github.com/masu-mi/face_detector/controllers"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
